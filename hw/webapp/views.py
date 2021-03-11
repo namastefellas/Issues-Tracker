42c92d85b98c0ff9024d1f27cd8c5e6d12b96bdc
German <coolmatoff@gmail.com>
2021-03-11 16:08:05 +0600

    created fixtures

--- hw/webapp/views.py
+++ hw/webapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from webapp.models import Task
+from django.views.generic import View, TemplateView, RedirectView
 
 # Create your views here.
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+    
+    
