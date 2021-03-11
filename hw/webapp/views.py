416aa5108f81a94e9b1273550ad49e9516a6838b
German <coolmatoff@gmail.com>
2021-03-11 16:36:34 +0600

    Added css, list of tasks

--- hw/webapp/views.py
+++ hw/webapp/views.py
@@ -7,4 +7,6 @@ from django.views.generic import View, TemplateView, RedirectView
 class IndexView(TemplateView):
     template_name = 'index.html'
     
-    
+    def get_context_data(self, **kwargs):
+        kwargs['tasks'] = Task.objects.all()
+        return super().get_context_data(**kwargs)
